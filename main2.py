6815f3f2d048c25e369a81f64952cc4ec2c8c7fe
Sinchan <sinchan.tex@gmail.com>
2023-01-05 21:45:08 +0530

    qr display canvas

--- main2.py
+++ main2.py
@@ -1,13 +1,12 @@
 import qrcode
-from tkinter import Tk, Frame, Label, Entry, Button
-from tkinter import messagebox
+from tkinter import Tk, Frame, Label, Entry, Button, messagebox, Canvas
 
 # from PIL import ImageTk, Image
 
 # Creating the window
 wn = Tk()
 wn.title('QR Code Generator')
-wn.geometry('400x400')
+wn.geometry('400x500')
 wn.config(bg='slate blue')
 
 
@@ -30,14 +29,14 @@ def generateCode():
 # c.pack()
 
 # Label for the window
-headingFrame = Frame(wn, bg="bisque", bd=5)
+headingFrame = Frame(wn, bg="grey", bd=5)
 headingFrame.place(relx=0.25, rely=0.05, relwidth=0.5, relheight=0.1)
 headingLabel = Label(headingFrame, text="Generate QR Code", bg='ghost white', font=('Helvetica', 14, 'bold'))
 headingLabel.place(relx=0, rely=0, relwidth=1, relheight=1)
 
 # Taking the input of the text or URL to get QR code
 Frame1 = Frame(wn, bg="slate blue")
-Frame1.place(relx=0.15, rely=0.15, relwidth=0.7, relheight=0.2)
+Frame1.place(relx=0.15, rely=0.15, relwidth=0.7, relheight=0.15)
 label1 = Label(Frame1, text="Enter the text/URL: ", bg="slate blue", fg='azure', font=('Helvetica', 9, 'bold'))
 label1.place(relx=0.05, rely=0.2, relheight=0.2)
 text = Entry(Frame1, font='Helvetica 9')
@@ -45,7 +44,7 @@ text.place(relx=0.05, rely=0.4, relwidth=0.9, relheight=0.3)
 
 # Getting input of the location to save QR Code
 Frame2 = Frame(wn, bg="slate blue")
-Frame2.place(relx=0.15, rely=0.3, relwidth=0.7, relheight=0.2)
+Frame2.place(relx=0.15, rely=0.25, relwidth=0.7, relheight=0.15)
 label2 = Label(Frame2, text="Enter the location to save the QR Code: ", bg="slate blue", fg='azure',font=('Helvetica', 9, 'bold'))
 label2.place(relx=0.05, rely=0.2, relheight=0.2)
 loc = Entry(Frame2, font='Helvetica 9')
@@ -53,7 +52,7 @@ loc.place(relx=0.05, rely=0.4, relwidth=0.9, relheight=0.3)
 
 # Getting input of the QR Code image name
 Frame3 = Frame(wn, bg="slate blue")
-Frame3.place(relx=0.15, rely=0.45, relwidth=0.7, relheight=0.2)
+Frame3.place(relx=0.15, rely=0.35, relwidth=0.7, relheight=0.15)
 label3 = Label(Frame3, text="Enter the name of the QR Code: ", bg="slate blue", fg='azure',font=('Helvetica', 9, 'bold'))
 label3.place(relx=0.05, rely=0.2, relheight=0.2)
 name = Entry(Frame3, font='Helvetica 9')
@@ -61,17 +60,24 @@ name.place(relx=0.05, rely=0.4, relwidth=0.9, relheight=0.3)
 
 # Getting the input of the size of the QR Code
 Frame4 = Frame(wn, bg="slate blue")
-Frame4.place(relx=0.15, rely=0.6, relwidth=0.7, relheight=0.2)
+Frame4.place(relx=0.15, rely=0.45, relwidth=0.7, relheight=0.15)
 label4 = Label(Frame4, text="Enter the size from 1 to 40 with 1 being 21x21: ", bg="slate blue", fg='azure', font=('Helvetica', 9, 'bold'))
 label4.place(relx=0.05, rely=0.2, relheight=0.2)
 size = Entry(Frame4, font='Helvetica 9')
-size.place(relx=0.05, rely=0.4, relwidth=0.5, relheight=0.3)
+size.place(relx=0.25, rely=0.4, relwidth=0.5, relheight=0.3)
 
 # Button to generate and save the QR Code
 button = Button(wn, text='Generate Code', font='Helvetica 9', command=generateCode)
-button.place(relx=0.35, rely=0.8, relwidth=0.25, relheight=0.08)
+button.place(relx=0.37, rely=0.6, relwidth=0.25, relheight=0.06)
 
-print(label2)
+# QR Code Display
+Frame5 = Frame(wn, bg="slate blue")
+Frame5.place(relx=0.15, rely=0.66, relwidth=0.7, relheight=0.35)
+label5 = Label(Frame5, text="QR Display", bg="slate blue", fg='azure', font=('Helvetica', 9, 'bold'))
+label5.place(relx=0.38, rely=0.0, relheight=0.2)
+canvas = Canvas(Frame5, bg='white', height=75, width=75)
+canvas.place(relx=0.25, rely=0.15, relwidth=0.5, relheight=0.8)
+
 
 # img = ImageTk.PhotoImage(Image.open("D:\pyprogs\Projects\qr-maker\hola.png"))
 # c.create_image(20, 20, anchor=NW, image=img)
