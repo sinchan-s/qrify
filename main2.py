56b4931de45b77e9b861cd8bec49f1ab946626b1
Sinchan <sinchan.tex@gmail.com>
2023-01-08 23:45:10 +0530

    release #0.2

--- main2.py
+++ main2.py
@@ -1,5 +1,5 @@
 import qrcode
-from tkinter import Tk, Frame, Label, Entry, Button, messagebox, Canvas
+from tkinter import Tk, Frame, Label, Entry, Button, messagebox, Canvas, filedialog
 from PIL import ImageTk, Image
 
 # Creating the window
@@ -8,25 +8,34 @@ wn.title('QR Code Generator')
 wn.geometry('400x500')
 wn.config(bg='slate blue')
 
-
 # Function to generate the QR code and save it
 def generateCode():
     # Creating a QRCode object of the size specified by the user
-    qr = qrcode.QRCode(version=size.get(),
-                       box_size=10,
-                       border=5)
+    qr = qrcode.QRCode(version=size.get(), box_size=10, border=5)
+    qr.add_data(text.get())     # Adding the data to be encoded to the QRCode object
+    qr.make(fit=True)           # Making the entire QR Code space utilized
+    img = qr.make_image()       # Generating the QR Code
+    img = img.resize((150, 150), Image.LANCZOS)
+    qrimg = ImageTk.PhotoImage(img)
+    qrdisp = Label(Frame5, image=qrimg)
+    qrdisp.place(relx=0.25, rely=0.15, relwidth=0.5, relheight=0.8)
+
+
+def saveCode():
+    qr = qrcode.QRCode(version=size.get(), box_size=10, border=5)
     qr.add_data(text.get())  # Adding the data to be encoded to the QRCode object
     qr.make(fit=True)  # Making the entire QR Code space utilized
-    img = qr.make_image()  # Generating the QR Code
+    img = qr.make_image()
     fileDirec = loc.get() + '\\' + name.get()  # Getting the directory where the file has to be saved
     img.save(f'{fileDirec}.png')  # Saving the QR Code
-    # Showing the pop-up message on saving the file
     rawimg = Image.open(f'{fileDirec}.png')
-    rawimg = rawimg.resize((150, 150), Image.LANCZOS)
+    rawimg = rawimg.resize((150, 150))
     qrimg = ImageTk.PhotoImage(rawimg)
     qrdisp = Label(Frame5, image=qrimg)
     qrdisp.place(relx=0.25, rely=0.15, relwidth=0.5, relheight=0.8)
+    # Showing the pop-up message on saving the file
     messagebox.showinfo("QR Code Generator", "QR Code is saved successfully!")
+
 
 # Label for the window
 headingFrame = Frame(wn, bg="grey", bd=5)
@@ -45,7 +54,7 @@ text.place(relx=0.05, rely=0.4, relwidth=0.9, relheight=0.3)
 # Getting input of the location to save QR Code
 Frame2 = Frame(wn, bg="slate blue")
 Frame2.place(relx=0.15, rely=0.25, relwidth=0.7, relheight=0.15)
-label2 = Label(Frame2, text="Your Saving Location", bg="slate blue", fg='azure',font=('Helvetica', 9, 'bold'))
+label2 = Label(Frame2, text="Your Saving Location", bg="slate blue", fg='azure', font=('Helvetica', 9, 'bold'))
 label2.place(relx=0.05, rely=0.2, relheight=0.2)
 loc = Entry(Frame2, font='Helvetica 9')
 loc.place(relx=0.05, rely=0.4, relwidth=0.9, relheight=0.3)
@@ -53,7 +62,7 @@ loc.place(relx=0.05, rely=0.4, relwidth=0.9, relheight=0.3)
 # Getting input of the QR Code image name
 Frame3 = Frame(wn, bg="slate blue")
 Frame3.place(relx=0.15, rely=0.35, relwidth=0.7, relheight=0.15)
-label3 = Label(Frame3, text="Name your QR Code", bg="slate blue", fg='azure',font=('Helvetica', 9, 'bold'))
+label3 = Label(Frame3, text="Name your QR Code", bg="slate blue", fg='azure', font=('Helvetica', 9, 'bold'))
 label3.place(relx=0.05, rely=0.2, relheight=0.2)
 name = Entry(Frame3, font='Helvetica 9')
 name.place(relx=0.05, rely=0.4, relwidth=0.9, relheight=0.3)
@@ -61,14 +70,28 @@ name.place(relx=0.05, rely=0.4, relwidth=0.9, relheight=0.3)
 # Getting the input of the size of the QR Code
 Frame4 = Frame(wn, bg="slate blue")
 Frame4.place(relx=0.15, rely=0.45, relwidth=0.7, relheight=0.15)
-label4 = Label(Frame4, text="Enter size: 1 to 40 (1 = 21x21)", bg="slate blue", fg='azure', font=('Helvetica', 9, 'bold'))
+label4 = Label(Frame4, text="Enter size: 1 to 40 (1 = 21x21)", bg="slate blue", fg='azure',
+               font=('Helvetica', 9, 'bold'))
 label4.place(relx=0.05, rely=0.2, relheight=0.2)
 size = Entry(Frame4, font='Helvetica 9')
 size.place(relx=0.25, rely=0.4, relwidth=0.5, relheight=0.3)
 
-# Button to generate and save the QR Code
+# Button to generate QR Code
 button = Button(wn, text='Generate Code', font='Helvetica 9 bold', command=generateCode)
-button.place(relx=0.37, rely=0.6, relwidth=0.25, relheight=0.06)
+button.place(relx=0.20, rely=0.6, relwidth=0.25, relheight=0.06)
+
+# Button to save QR Code
+savbutton = Button(wn, text='Save Code', font='Helvetica 9 bold', command=saveCode)
+savbutton.place(relx=0.55, rely=0.6, relwidth=0.25, relheight=0.06)
+
+'''
+filename = filedialog.asksaveasfilename(initialdir='/', title='Save File', filetypes=(('Text Files', 'txt.*'), ('All Files', '*.*')))
+textContent = "I'm the text in the file"
+myfile = open(filename, "w+")
+myfile.write(textContent)
+# to test
+print("File saved as ", filename)
+'''
 
 # QR Code Display
 Frame5 = Frame(wn, bg="slate blue")
